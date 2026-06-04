d2f0d88707bd77379ad32741d80d08a3c319fc52
Faheema Tamton <fymatampc@gmail.com>
2026-06-04 08:38:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ gsap.from(".hero h1", {
 
 // SECTION ANIMATION
 
+document.querySelectorAll(".section").forEach(section => {
+  gsap.from(section, {
+    scrollTrigger: section,
+    y: 50,
+    opacity: 0,
+    duration: 1
+  });
+});
+
 
 
 
