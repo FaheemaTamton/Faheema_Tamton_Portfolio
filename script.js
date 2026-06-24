48701b108a95d43008dbe6741ceb7746a3d31212
FaheemaTamton <fymatampc@gmail.com>
2026-06-24 21:22:08 +0530

    Update portfolio

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ gsap.from(".hero h1", {
   duration: 1
 });
 
-// SECTION ANIMATION
-
-document.querySelectorAll(".section").forEach(section => {
-  gsap.from(section, {
-    scrollTrigger: section,
-    y: 50,
-    opacity: 0,
-    duration: 1
-  });
-});
 
 
 
